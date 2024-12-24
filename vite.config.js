4b2fd25d93648eb7e918da892bafbe5aaa4c53a0
arsalanlal414 <muhammad.arsalan@soc-solution.com>
2024-12-24 16:12:47 +0500

    vercel issue

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   server: {
-    historyApiFallback: true, // For dev server fallback
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    watch: {
+      usePolling: true,
     },
   },
 });
